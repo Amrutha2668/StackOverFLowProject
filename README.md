8c2db18a6f4c27fa3d6b14e3d851a79c7fecde72
Amrutha N <amruthan2668@gmail.com>
2021-09-22 12:19:06 +0530

    Backend-readme-commit

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
+# About Project
+## Backend
+1. Creation of DataBase if doesn't exists.
+2. Connecting to the Created DataBase.
+3. Table creation if doesn't exists.
+4. Fetching data from sign-up page.
+5. Updating that values to table.  
+
 # About npm's 
 ### In order to avoid restarting of the server nodemon module is used in the project
-1. nodemon - npm run watch
+1. nodemon(to run use the given command) - npm run watch
 2. watch script added into the package.json
 3. watch is the script name given to nodemon.
--- app.js
+++ app.js
@@ -14,13 +14,13 @@ app.listen(3007, function () {
 //middleware functions execute during lifecycle of a request to the express server  EG:USE
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/signup", (req, res) => {
-  //sending a whole html file here
+app.get("/signUp", (req, res) => {
+  //sending a whole html file to browser
   res.sendFile(path.join(__dirname, "./public/main.html"));
 });
 
 //if data is submitted we are giving post method then
-app.post("/signup", function (req, res) {
+app.post("/signUp", function (req, res) {
   apiResponse = req.body;
   console.log(apiResponse);
   let records = [];
@@ -30,22 +30,25 @@ app.post("/signup", function (req, res) {
       Name: apiResponse.name,
       Password: apiResponse.psw,
       Gender: apiResponse.gender,
-    })
+    });
     res.send("account created");
   } else {
     res.send("Password Doesn't match");
   }
+
+  // Pushing the user i/p data to table in Db.
   User.create({
     Email: apiResponse.email,
     Name: apiResponse.name,
     Password: apiResponse.psw,
     Gender: apiResponse.gender,
-  })
+  });
   console.log("Records Updated!!");
 });
 
 createTable();
 
+// Table creation in db if table does not exists.
 async function createTable() {
   await sequelize
     .sync()
