76916c4f751e8bb33bfcd71c7e5a5bdaff0b716d
Amrutha N <amruthan2668@gmail.com>
2021-09-23 16:57:29 +0530

    Revert "main-file-commit"
    This reverts commit 69c8e57f2aeb50e7ef01cda3bab14a892a21f2ef.

--- database/database.js
+++ database/database.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // eslint-disable-next-line no-undef
 const { Sequelize } = require("sequelize");
 
@@ -19,3 +20,27 @@ sequelize = new Sequelize("StackOverFlow", "root", "root@123", {
 
 // eslint-disable-next-line no-undef
 module.exports = sequelize;
+||||||| 72226bd
+=======
+// eslint-disable-next-line no-undef
+const { Sequelize } = require("sequelize");
+
+// Access to Mysql server
+let sequelize = new Sequelize("", "root", "varSHA_96", {
+  dialect: "mysql",
+});
+
+// Database creation
+sequelize
+  .query("CREATE DATABASE IF NOT EXISTS `StackOverFlow`;")
+  .then(() => console.log("Created Database"));
+
+// Establishing connection with created db
+sequelize = new Sequelize("StackOverFlow", "root", "varSHA_96", {
+  host: "localhost",
+  dialect: "mysql",
+});
+
+// eslint-disable-next-line no-undef
+module.exports = sequelize;
+>>>>>>> 646eb4ead6fec55961bb1912b18920346f4a2434
